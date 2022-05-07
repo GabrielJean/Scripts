901260322135f97caedb894c3951725a39416b53
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2022-05-07 17:23:16 -0400

    updated docker-compose

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ then
         sudo sh get-docker.sh
 
         echo "Install Docker-Compose"
-        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
 
 fi
